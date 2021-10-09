4f249ad7d94fbc246214b7435f7aae6f9004118a
koory1st <32436334@qq.com>
2021-10-09 19:24:53 +0800

    use unicode-segmentation to get graphemes

--- src/q05.rs
+++ src/q05.rs
@@ -1,4 +1,5 @@
 // http://llever.com/exercism-rust-zh/nth-prime/README.zh.html
+use unicode_segmentation::UnicodeSegmentation;
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    input.to_owned().graphemes(true).rev().collect()
 }
