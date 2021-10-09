3c8be985bb2715c703e632a923066f8f4347c28c
koory1st <32436334@qq.com>
2021-10-09 23:13:40 +0800

    update q06 for performance

--- src/q06.rs
+++ src/q06.rs
@@ -1,46 +1,33 @@
 // http://llever.com/exercism-rust-zh/nth-prime/README.zh.html
-pub fn nth(n: u32) -> u32 {
-    let first = Prime { curr: 2 };
 
+pub fn nth(n: u32) -> u32 {
     if n == 0 {
-        return first.curr;
+        return 2;
     }
 
-    let a = first.skip(n as usize - 1).next().unwrap();
-
-    return a;
-}
-
-struct Prime {
-    curr: u32,
-}
-
-impl Iterator for Prime {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        for num in self.curr + 1.. {
-            if num % 2 == 0 {
-                continue;
+    let mut num = 3u32;
+    let mut index = 0u32;
+    loop {
+        if is_prime(num) {
+            index += 1;
+            if index == n {
+                return num;
             }
-
-            let mut has_factor: bool = false;
-            for j in 2..num {
-                if num % j == 0 {
-                    has_factor = true;
-                    break;
-                }
-            }
-
-            if has_factor {
-                continue;
-            }
-
-            self.curr = num;
-
-            return Some(self.curr);
         }
 
-        None
+        num += 2;
     }
+}
+
+fn is_prime(num: u32) -> bool {
+    let mut f = 2u32;
+
+    while f * f <= num {
+        if num % f == 0 {
+            return false;
+        }
+
+        f += 1;
+    }
+    return true;
 }
