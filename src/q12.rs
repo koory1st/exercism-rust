add4e0b29eac4521ee13d1a6ce83688f47db1f66
koory1st <32436334@qq.com>
2021-10-26 21:19:38 +0800

    resolve q12

--- src/q12.rs
+++ src/q12.rs
@@ -1,8 +1,19 @@
 // http://llever.com/exercism-rust-zh/grains/README.zh.html
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s == 1 {
+        return 1;
+    }
+
+    let mut ret = 1u64;
+    for _ in 2..=s {
+        ret *= 2;
+    }
+
+    ret
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=64)
+        .map(|x| { square(x) })
+        .sum()
 }
