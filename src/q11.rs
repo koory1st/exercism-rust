22341edc12195db371d6edb0bca3e62a0e2dded8
koory1st <32436334@qq.com>
2021-10-26 20:50:57 +0800

    resolve q11

--- src/q11.rs
+++ src/q11.rs
@@ -1,8 +1,11 @@
 // http://llever.com/exercism-rust-zh/sum-of-multiples/README.zh.html
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    if factors.len() == 0 {
+        return 0;
+    }
+
+    // 首先列出 1 到 limit, 然后看每个数能否被 factors 里的数整除
+    (1..limit).filter(|x| {
+        factors.iter().any(|y| { *x % *y == 0 })
+    }).sum()
 }
