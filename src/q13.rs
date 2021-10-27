7f95c53e040a4a46afd81950709943b0d3486e5f
koory1st <32436334@qq.com>
2021-10-27 20:34:45 +0800

    remove a loop of c

--- src/q13.rs
+++ src/q13.rs
@@ -1,11 +1,10 @@
 // http://llever.com/exercism-rust-zh/pythagorean-triplet/README.zh.html
 pub fn find() -> Option<u32> {
-    for a in 3u32..1000 - 3 {
-        for b in a + 1..1000 - 2 {
-            for c in b + 1..1000 - 1 {
-                if a * a + b * b == c * c && a + b + c == 1000 {
-                    return Some(a * b * c);
-                }
+    for a in 3u32..(1000 - 3) {
+        for b in (a + 1)..(1000 - a) {
+            let c = 1000 - a - b;
+            if a * a + b * b == c * c {
+                return Some(a * b * c);
             }
         }
     }
