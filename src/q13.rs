37644aab1dea6a1bc65f86c7c372e53fa1a8b9f0
koory1st <32436334@qq.com>
2021-10-27 20:31:31 +0800

    resovle q13

--- src/q13.rs
+++ src/q13.rs
@@ -1,4 +1,13 @@
 // http://llever.com/exercism-rust-zh/pythagorean-triplet/README.zh.html
 pub fn find() -> Option<u32> {
-    unimplemented!();
+    for a in 3u32..1000 - 3 {
+        for b in a + 1..1000 - 2 {
+            for c in b + 1..1000 - 1 {
+                if a * a + b * b == c * c && a + b + c == 1000 {
+                    return Some(a * b * c);
+                }
+            }
+        }
+    }
+    None
 }
