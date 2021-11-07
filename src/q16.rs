7b022c956d425959e69136bada3717894721012d
koory1st <32436334@qq.com>
2021-11-07 20:22:41 +0800

    finish q16

--- src/q16.rs
+++ src/q16.rs
@@ -1,4 +1,21 @@
 // http://llever.com/exercism-rust-zh/armstrong-numbers/README.zh.html
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let string = num.to_string();
+    let chars = string.chars();
+    let count = string.len() as u32;
+
+    let mut sum = 0u32;
+    for x in chars {
+        match x.to_string().parse::<u32>() {
+            Ok(n) => {
+                sum += n.pow(count)
+            }
+            _ => {}
+        };
+    }
+
+    if sum == num {
+        return true;
+    }
+    false
 }
